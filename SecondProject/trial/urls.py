7043c9140d88ca182086ebbbec3b73ef93a61d81
Harsh Agrawal <harsh.agrawal17@siesgst.ac.in>
2019-09-07 15:23:48 +0530

    Done with second Project

--- SecondProject/trial/urls.py
+++ SecondProject/trial/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 
 path("",views.index,name="index"),
+path("submit/",views.submit,name="index"),
 
 ]
--- SecondProject/trial/views.py
+++ SecondProject/trial/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return render(request, "index.html")
+	straa= "This text doesn't exist in HTML, it has been passed by views and variables"	
+	return render(request, "index.html", {"text" : straa })
+
+def submit(request):
+	name = request.GET["username"]
+	return HttpResponse(name)
